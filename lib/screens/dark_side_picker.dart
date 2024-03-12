68d378395fc0e9b446edc1116171e1c0e6ec81ac
notrayo <rayoleeroy@gmail.com>
2024-03-12 15:30:08 +0300

    finished LS and DS UI

--- lib/screens/dark_side_picker.dart
+++ lib/screens/dark_side_picker.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hvv_hero_picker/widgets/faction_button.dart';
 
 class DarkSideScreen extends StatefulWidget {
   const DarkSideScreen({super.key});
@@ -11,8 +12,49 @@ class _DarkSideScreenState extends State<DarkSideScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('DARK SIDE'),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 1, 1, 37),
+          Color.fromARGB(250, 194, 4, 4),
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 70),
+              const Text(
+                'CHOOSE YOUR VILLAIN',
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400),
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+              Image.asset(
+                'assets/villains_images/Battlefront_Vader.jpg',
+                height: 350,
+                width: 350,
+              ),
+              const SizedBox(
+                height: 45,
+              ),
+              const Text(
+                'DARTH VADER',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              FactionButton(buttonText: 'SWITCH VILLAIN', onButtonPress: () {})
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/screens/light_side_picker.dart
+++ lib/screens/light_side_picker.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hvv_hero_picker/widgets/faction_button.dart';
 
 class LightSideScreen extends StatefulWidget {
   const LightSideScreen({super.key});
@@ -11,8 +12,50 @@ class _LightSideScreenState extends State<LightSideScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('LIGHT SIDE'),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 2, 2, 98),
+          Color.fromARGB(250, 66, 94, 255),
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: Center(
+            child: Column(
+          children: [
+            const SizedBox(
+              height: 70,
+            ),
+            const Align(
+              alignment: Alignment.topCenter,
+              child: Text(
+                'CHOOSE YOUR HERO',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400),
+              ),
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            Image.asset(
+              'assets/heroes_images/Battlefront_Luke.jpg',
+              height: 350,
+              width: 350,
+            ),
+            const SizedBox(
+              height: 35,
+            ),
+            const Text(
+              'LUKE SKYWALKER',
+              style: TextStyle(color: Colors.yellow, fontSize: 27),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            FactionButton(buttonText: 'SWITCH HERO', onButtonPress: () {})
+          ],
+        )),
       ),
     );
   }
